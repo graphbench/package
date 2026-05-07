d648c5efcdce7085c0356b0d60097380e9dd8879
Arman Mielke <arman.mielke@yahoo.de>
2026-05-07 17:20:10 +0200

    Mark SplitStrategy as abstract

--- graphbench/_loader/_split_strategies/_split_strategy.py
+++ graphbench/_loader/_split_strategies/_split_strategy.py
@@ -1,11 +1,11 @@
-
+from abc import ABC, abstractmethod
 from typing import Callable, Dict, Optional, TypeAlias
 
 
 DatasetFactory: TypeAlias = Callable[[str, str, Optional[str]], object]
 
 
-class SplitStrategy:
+class SplitStrategy(ABC):
+    @abstractmethod
     def build(self, factory: DatasetFactory, dataset_name: str) -> Dict[str, object]:
-        raise NotImplementedError
-
+        ...
